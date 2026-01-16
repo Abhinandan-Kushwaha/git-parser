27876cbcc821fb5f427a70f3baa63c971b474fd1
Abhinandan Kushwaha <kushabhi5@gmail.com>
2026-01-17 02:16:49 +0530

    handled null pointers

--- parsegit.py
+++ parsegit.py
@@ -32,48 +32,52 @@ async def crawlPage(session, baseUrl, relPath, branch):
     for item in filesArray:
         name = item.get("name")
         contentType = item.get("contentType")
+        if contentType:
+            if contentType == "directory":
+                if relPath == "":
+                    newRelPath = f"/tree/{branch}/" + name
+                else:
+                    newRelPath = relPath + "/" + name
 
-        if contentType == "directory":
-            if relPath == "":
-                newRelPath = f"/tree/{branch}/" + name
+                await crawlPage(session, baseUrl, newRelPath, branch)
             else:
-                newRelPath = relPath + "/" + name
-
-            await crawlPage(session, baseUrl, newRelPath, branch)
-        else:
-            path = baseUrl +f"/blob/{branch}"+ relPath + "/" + name
-            if len(relPath)>0:
-                print(relPath + "/" + name)
-            else:
-                print(name)
-            async with session.get(path) as response:
-                content = await response.text()
-            if path.endswith(".py"):
-                delimiter = "data-target=\"react-app.embeddedData\">"
-                startIndex = content.find(delimiter) + len(delimiter)
-                codeStr = content[startIndex:-1]
-                endIndex = codeStr.find("</script>")
-                codeStr = codeStr[0:endIndex]
-                codeObj = json.loads(codeStr)
-                code = codeObj.get("payload").get("blob").get("rawLines")
-                print("_______________________________")
-                print(f"Methods and classes in {name}-")
-                for line in code:
-                    x = line.strip()
-                    if x.startswith("def "):
-                        x=line.find("(")
-                        # There is a corner case, if there is multiline string and If a line starts with def then it will also count.
-                        y=line[4:x]
-                        z=y.strip()
-                        print("method: ",z)
-                    elif x.startswith("Class"):
-                        x=line.find("(")
-                        y=line[5:x]
-                        z=y.strip()
-                        print("class: ",z)
-                print("_______________________________")
-                print("")
-                print("")
+                path = baseUrl +f"/blob/{branch}"+ relPath + "/" + name
+                if len(relPath)>0:
+                    print(relPath + "/" + name)
+                else:
+                    print(name)
+                async with session.get(path) as response:
+                    content = await response.text()
+                if path.endswith(".py"):
+                    delimiter = "data-target=\"react-app.embeddedData\">"
+                    startIndex = content.find(delimiter) + len(delimiter)
+                    codeStr = content[startIndex:-1]
+                    endIndex = codeStr.find("</script>")
+                    codeStr = codeStr[0:endIndex]
+                    codeObj = json.loads(codeStr)
+                    payload = codeObj.get("payload")
+                    if payload:
+                        blob = payload.get("blob")
+                        if blob:
+                            code = blob.get("rawLines")
+                            print("_______________________________")
+                            print(f"Methods and classes in {name}-")
+                            for line in code:
+                                x = line.strip()
+                                if x.startswith("def "):
+                                    x=line.find("(")
+                                    # There is a corner case, if there is multiline string and If a line starts with def then it will also count.
+                                    y=line[4:x]
+                                    z=y.strip()
+                                    print("method: ",z)
+                                elif x.startswith("Class"):
+                                    x=line.find("(")
+                                    y=line[5:x]
+                                    z=y.strip()
+                                    print("class: ",z)
+                            print("_______________________________")
+                            print("")
+                            print("")
             
 
 
